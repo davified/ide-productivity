ded3915ef8bcb4f5afd7346ba00462f007886f3b
davidtan <davidtan@thoughtworks.com>
2023-10-06 00:44:46 +1100

    Align exercises with content in chapter 6

--- src/ide_productivity/2_refactoring.py
+++ src/ide_productivity/2_refactoring.py
@@ -8,16 +8,12 @@ from src.ide_productivity.helpers import do_something
 
 df = pd.DataFrame(
     {
-        "name": ['Alice', 'Bob', 'Charlie'],
+        "name": ['Alice', "Bob", 'Charlie'],
         'seat_number': ['1A', '10C', '42G'], 'age': ['30', '45', '12'],
     }
 )
 
 ### 2. Refactoring ###
-# Exercise: Format code
-# TODO: Hit shortcut for Format Document
-# Notice how the code style is now consistent (e.g. no more mix of single quotes and double quotes)
-
 
 # Exercise: Rename variable
 # TODO: Rename `df` to something more meaningful (e.g. `passengers`)
@@ -26,8 +22,19 @@ print(df)
 do_something()  # FIXME: enable the renaming of this variable
 
 
-# Exercise: Extract variable / method / function
+# Exercise: Extract method
+# TODO: Select the last 4 lines of code in this exercise, hit the shortcut for extract method, and give the function a name (e.g. add_prettified_ticket_column)
+prettified_name = "Passenger: " + df["name"]
+prettified_seat_number = "Seat Number: " + df["seat_number"]
+df["prettified_ticket"] = prettified_name + ", " + prettified_seat_number
+print(df)
 
+
+
+# Exercise: Fix coding formatting
+# TODO: Notice the formatting inconsistencies (e.g. mixed single and double quotes in df)? Hit the shortcut to format the code.
+# Notice how the code style is now consistent (e.g. no more mix of single quotes and double quotes)
 
 
 # Exercise: Sort imports
+# TODO: Notice the unused imports at the top? Hit the shortcut the organize imports, and notice the imports are now tidy
