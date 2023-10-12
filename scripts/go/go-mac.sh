0b84081aec1339730f644fc79966bf1cd584c770
David Tan <davidtan@thoughtworks.com>
2023-10-12 15:32:55 +1100

    Fix lefthook installation for Mac

--- scripts/go/go-mac.sh
+++ scripts/go/go-mac.sh
@@ -8,5 +8,11 @@ which brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homeb
 echo "Installing Python 3 if it's not installed..."
 which python3 || brew install python3
 
+echo "Installing lefthook if it's not installed..."
+which lefthook || brew install lefthook
+
+echo "Setting up git hooks for this project"
+lefthook install
+
 echo "Installing dependencies on host (so that we can configure a virtual environment for our IDE)"
 ./scripts/go/install-dependencies-on-host.sh
