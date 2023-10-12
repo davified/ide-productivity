13fceff7f3688fd388a8459fcba194490c031ad8
David Tan <davidtan@thoughtworks.com>
2023-10-12 15:20:55 +1100

    Fix lefthook install in linux go script

--- scripts/go/go-linux-ubuntu.sh
+++ scripts/go/go-linux-ubuntu.sh
@@ -11,5 +11,12 @@ which python3 || sudo apt install -y python3
 echo "Installing pip if it's not installed..."
 which pip || sudo apt install -y python3-pip
 
+echo "Installing lefthook if it's not installed..."
+which lefthook || curl -1sLf 'https://dl.cloudsmith.io/public/evilmartians/lefthook/setup.deb.sh' | sudo -E bash
+which lefthook || sudo apt install lefthook
+
+echo "Setting up git hooks for this project"
+lefthook install
+
 echo "Installing dependencies on host..."
 ./scripts/go/install-dependencies-on-host.sh
--- scripts/go/install-dependencies-on-host.sh
+++ scripts/go/install-dependencies-on-host.sh
@@ -11,10 +11,4 @@ poetry config virtualenvs.in-project false
 echo "Installing dependencies..."
 poetry install
 
-echo "Installing lefthook (for git hooks) if it's not installed..."
-which lefthook || python3 -m pip install --user lefthook
-
-echo "Setting up git hooks for this project"
-lefthook install
-
 echo "Done. Configure your IDE to use the following virtual environment path: $(poetry env info -p)/bin/python"
