917169550a49d1994b68c1fb34c0e15e388ae1bc
Nhung121 <nhn121@yahoo.com>
2023-10-12 15:29:07 +1100

    Fix windows go script

--- scripts/go/go-windows.bat
+++ scripts/go/go-windows.bat
@@ -2,7 +2,7 @@ where pipx
 IF %ERRORLEVEL% EQU 0 (ECHO "pipx is installed") ELSE (python -m pip install --user pipx)
 
 where poetry
-IF %ERRORLEVEL% EQU 0 (ECHO "poetry is installed") ELSE (pipx install poetry)
+IF %ERRORLEVEL% EQU 0 (ECHO "poetry is installed") ELSE (pip install poetry)
 
 poetry config virtualenvs.in-project false
 
@@ -10,7 +10,7 @@ ECHO "Installing dependencies..."
 poetry install
 
 where lefthook
-IF %ERRORLEVEL% EQU 0 (ECHO "lefthook is installed") ELSE (pipx install lefthook)
+IF %ERRORLEVEL% EQU 0 (ECHO "lefthook is installed") ELSE (winget install evilmartians.lefthook)
 
 ECHO "Setting up git hooks for this project"
 lefthook install
