a5009c3d17013c14a3998742ed814dc7ffb31282
David Tan <davidtan@thoughtworks.com>
2023-10-12 14:57:33 +1100

    Install lefthook in go scripts and run formatting and linting checks in precommit hook

--- scripts/go/go-windows.bat
+++ scripts/go/go-windows.bat
@@ -2,12 +2,18 @@ where pipx
 IF %ERRORLEVEL% EQU 0 (ECHO "pipx is installed") ELSE (python -m pip install --user pipx)
 
 where poetry
-IF %ERRORLEVEL% EQU 0 (ECHO "poetry is installed") ELSE (pip3 install poetry)
+IF %ERRORLEVEL% EQU 0 (ECHO "poetry is installed") ELSE (pipx install poetry)
 
 poetry config virtualenvs.in-project false
 
 ECHO "Installing dependencies..."
 poetry install
+
+where lefthook
+IF %ERRORLEVEL% EQU 0 (ECHO "lefthook is installed") ELSE (pipx install lefthook)
+
+ECHO "Setting up git hooks for this project"
+lefthook install
 
 ECHO "Done.  Configure your IDE to use the .\Scripts\python.exe interpreter in the following virtual environment path"
 poetry env info -p
--- scripts/go/install-dependencies-on-host.sh
+++ scripts/go/install-dependencies-on-host.sh
@@ -11,4 +11,10 @@ poetry config virtualenvs.in-project false
 echo "Installing dependencies..."
 poetry install
 
+echo "Installing lefthook (for git hooks) if it's not installed..."
+which lefthook || python3 -m pip install --user lefthook
+
+echo "Setting up git hooks for this project"
+lefthook install
+
 echo "Done. Configure your IDE to use the following virtual environment path: $(poetry env info -p)/bin/python"
